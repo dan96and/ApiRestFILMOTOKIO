5d3678758edc4bfaa76cb13e6a316e935bdb2e57
Daniel <d.andres.bacho@gmail.com>
2023-08-22 14:10:17 +0200

    Implementación de inicio de proyecto

---
+++ src/main/java/com/example/apirestfilmotokio/ApiRestFilmotokioApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestfilmotokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFilmotokioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestFilmotokioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apirestfilmotokio/ApiRestFilmotokioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestfilmotokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFilmotokioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
